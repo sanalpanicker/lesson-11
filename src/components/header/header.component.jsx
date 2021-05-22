c5fa452cab4c3fbf366196c76155ee34b2c11ce5
Dev Nerd <dev.nerd87@gmail.com>
2021-05-22 16:31:06 -0700

    feat: Reduxifying the app

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
@@ -8,7 +8,9 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
+
   <div className='header'>
+    {console.log(currentUser)}
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
     </Link>
@@ -24,12 +26,16 @@ const Header = ({ currentUser }) => (
           SIGN OUT
         </div>
       ) : (
-        <Link className='option' to='/signin'>
-          SIGN IN
-        </Link>
-      )}
+          <Link className='option' to='/signin'>
+            SIGN IN
+          </Link>
+        )}
     </div>
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
